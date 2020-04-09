a73fdfd0a937ee2278d0100e3f6bb39624e3b7fe
ES\agarwad1 <dheeraj.agarwal@adp.com>
2020-04-09 13:44:27 +0530

    First Change of BIT Mesra

---
+++ src/com/bit/mesra/client/BitMesraClient.java
@@ -0,0 +1,11 @@
+package com.bit.mesra.client;
+
+public class BitMesraClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wel come to BIT Mesra College App");
+
+	}
+
+}
