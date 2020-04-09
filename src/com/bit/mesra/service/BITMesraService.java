9abc85d25460bae95fb44b9eef3737b44f7362b9
dheeraj24091993 <agarwaldheeraj2020@gmail.com>
2020-04-09 13:59:00 +0530

    Second change

---
+++ src/com/bit/mesra/service/BITMesraService.java
@@ -0,0 +1,19 @@
+package com.bit.mesra.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BITMesraService {
+	
+	public List<String> getAllStudents() {
+		List<String> mesraStudents = new ArrayList<>();
+		mesraStudents.add("Vaishali S");
+		mesraStudents.add("Prachi B");
+		mesraStudents.add("Rohini C");
+		
+		return mesraStudents;
+		
+		
+	}
+
+}
